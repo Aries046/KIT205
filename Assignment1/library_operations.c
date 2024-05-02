a7c628f3528e7f5323caa64f71fe467ae7f8e588
Hongchao Xue <132851038+Aries046@users.noreply.github.com>
2024-05-03 00:45:24 +0800

    Implemented a binary search tree

--- Assignment1/library_operations.c
+++ Assignment1/library_operations.c
@@ -10,8 +10,17 @@ void add_book(Library *lib, int book_id, const char *title, const char *author) 
     new_book->book_id = book_id;
     strcpy(new_book->title, title);
     strcpy(new_book->author, author);
-    new_book->next = lib->books;
-    lib->books = new_book;
+    new_book->left = new_book->right = NULL;
+
+    Book **ptr = &(lib->books);
+    while (*ptr) {
+        Book *current = *ptr;
+        if (book_id < current->book_id)
+            ptr = &current->left;
+        else
+            ptr = &current->right;
+    }
+    *ptr = new_book;
 }
 
 void add_borrower(Library *lib, int borrower_id, const char *name) {
@@ -21,11 +30,15 @@ void add_borrower(Library *lib, int borrower_id, const char *name) {
     new_borrower->next = lib->borrowers;
     lib->borrowers = new_borrower;
 }
-Book* find_book(Library *lib, int book_id) {
-    Book *current = lib->books;
-    while (current) {
-        if (current->book_id == book_id) return current;
-        current = current->next;
+
+Book* find_book(Book *root, int book_id) {
+    while (root) {
+        if (book_id < root->book_id)
+            root = root->left;
+        else if (book_id > root->book_id)
+            root = root->right;
+        else
+            return root;
     }
     return NULL;
 }
