a5209a653f679b498bcd04ad82de66a11b81c24e
Hongchao Xue <132851038+Aries046@users.noreply.github.com>
2024-05-02 23:56:40 +0800

    Added function prototypes for library operations

--- Assignment1/library_operations.h
+++ Assignment1/library_operations.h
@@ -1,1 +1,13 @@
+#ifndef LIBRARY_OPERATIONS_H
+#define LIBRARY_OPERATIONS_H
 
+#include "data_structures.h"
+
+void init_library(Library *lib);
+void add_book(Library *lib, int book_id, const char *title, const char *author);
+void add_borrower(Library *lib, int borrower_id, const char *name);
+Book* find_book(Library *lib, int book_id);
+Borrower* find_borrower(Library *lib, int borrower_id);
+void print_library(Library *lib);
+
+#endif
