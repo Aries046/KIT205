77addf97f7da41c41d43a971657942d95509682d
Hongchao Xue <132851038+Aries046@users.noreply.github.com>
2024-07-17 19:48:53 +0800

    Add Main Menu and Entry Point
    Implement the main entry point and main menu logic.

--- Assignment2/main.c
+++ Assignment2/main.c
@@ -28,3 +28,41 @@ typedef struct {
 
 static Graph map;
 char PASSCODE[20] = "bao";
+int main() {
+    int n;
+    printf("\n\n\n");
+    printf("\t\t\t\tWelcome to the Tasmania Tourism Guide! All scenic spot information is fictional\n");
+    printf("\t\t\t\tDear kind and beautiful gentlemen and ladies, please select your identity\n");
+    printf("\t\t\t\t<1> To enter the admin interface, please enter the number 1\n");
+    printf("\t\t\t\t<2> To enter the tourist interface, please enter the number 2\n");
+    start:
+    scanf("%d", &n);
+    if (n == 1) {
+        char pc[20];
+        printf("\t\t\t\tPlease enter the admin password: ");
+        scanf("%s", pc);
+        getchar();
+        while (strcmp(pc, PASSCODE) != 0) {
+            printf("Incorrect password, please re-enter the password:\n");
+            scanf("%s", pc);
+        }
+        if (strcmp(pc, PASSCODE) == 0) {
+            printf("Identity verified, entering the system...");
+            menu1();
+        }
+    } else if (n == 2) {
+        menu2();
+    } else {
+        printf("Input error, please re-enter 1 or 2\n");
+        goto start;
+    }
+    return 0;
+}
+
+void menu1() {
+    // Placeholder for admin menu
+}
+
+void menu2() {
+    // Placeholder for tourist menu
+}
